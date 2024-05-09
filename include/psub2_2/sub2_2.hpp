92afd7228964f47ed65d056df2056054b14c10cc
Sungmyunghoon <112747810+Sungmyunghoon@users.noreply.github.com>
2024-05-09 11:01:23 +0900

    Update sub2_2.hpp

--- include/psub2_2/sub2_2.hpp
+++ include/psub2_2/sub2_2.hpp
@@ -1,16 +1,16 @@
-#ifndef _SUB_HPP_
+#ifndef _SUB_HPP_ //중복 방지
 #define _SUB_HPP_
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/vector3.hpp"
-#include <functional>
-#include <memory>
-using std::placeholders::_1;
-class Sub : public rclcpp::Node
+#include "rclcpp/rclcpp.hpp" //Ros2의 C++라이브러리를 사용하기 위한 헤더 파일을 포함합니다.
+#include "geometry_msgs/msg/vector3.hpp" // std_msgs 패키지의 vector형 메시지 타입을 사용하기 위한 헤더 파일을 사용합니다.
+#include <functional> // 함수 객체를 만들고 바인딩하기 위한 헤더 파일
+#include <memory> // make_shared 함수를 사용하기 위한 헤더파일
+using std::placeholders::_1; 
+class Sub : public rclcpp::Node // Sub클레스 선언
 {
 private:
-    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr sub_;
-    void subscribe_msg(const geometry_msgs::msg::Vector3::SharedPtr msg) const;
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr sub_;  // Vector 메시지를 구하기 위한 스마트 포인터
+    void subscribe_msg(const geometry_msgs::msg::Vector3::SharedPtr msg) const; //정수형 메시지를 구독할 때 호출되는 콜백 함수
 public:
-    Sub();
+    Sub(); // 생성자
 };
-#endif //_SUB_HPP_
+#endif //_SUB_HPP_ 헤더 파일 종료
