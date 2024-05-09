8f8ed681ad579fe26290884a89b2bf24dccc2c18
Sungmyunghoon <112747810+Sungmyunghoon@users.noreply.github.com>
2024-05-09 11:05:21 +0900

    Update smain.cpp

--- src/smain.cpp
+++ src/smain.cpp
@@ -1,9 +1,9 @@
-#include "psub2_2/sub2_2.hpp"
-int main(int argc, char * argv[])
+#include "psub2_2/sub2_2.hpp" // 헤더 파일 포함
+int main(int argc, char * argv[]) // main 함수
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<Sub>();
-    rclcpp::spin(node);
+    rclcpp::init(argc, argv); // ROS 2를 초기화
+    auto node = std::make_shared<Sub>(); // 클래스의 객체를 생성
+    rclcpp::spin(node); // 노드가 종료될 때까지 계속해서 실행
     rclcpp::shutdown();
     return 0;
 }
